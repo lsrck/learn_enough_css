504126ae4b57bfaf20dc345474a947019343dfaf
Louis Aurick <louisaurick@ntymail.com>
2018-08-30 12:21:51 +0200

    gallery images modified

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,11 @@
-function activateGallery() {
-  alert("hello from the gallery file");
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+  
+  let thumbnails = document.getElementById("gallery-thumbs").querySelectorAll("img");
+  let mainImage = document.getElementById("gallery-photo").querySelector("img")
+
+  thumbnails.forEach(function(thumbnail){
+    let newImageSrc = thumbnail.src.replace(/small/i,"large")
+    thumbnail.onClick = function() {mainImage.setAttribute("src",newImageSrc)};
+    console.log(newImageSrc);
+    });
+});
